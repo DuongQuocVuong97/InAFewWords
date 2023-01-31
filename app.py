21187bf4df39ef269e1476eb6b5c04278c8909b0
DuongQuocVuong97 <38318609+DuongQuocVuong97@users.noreply.github.com>
2023-01-31 12:49:41 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import re
+
+class SummarizationModel:
+    def __init__(self):
+        self.model_name = "csebuetnlp/mT5_multilingual_XLSum"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name)
+        self.WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
+
+    def summarize(self, text):
+        input_ids = self.tokenizer([self.WHITESPACE_HANDLER(text)],
+                                return_tensors="pt",
+                                padding="max_length",
+                                truncation=True,
+                                max_length=512)["input_ids"]
+        
+        output_ids = self.model.generate(input_ids=input_ids,
+                                        max_length=84,
+                                        no_repeat_ngram_size=2,
+                                        num_beams=4)[0]
+
+        summary = self.tokenizer.decode(
+            output_ids,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False
+        )
+
+        return summary
